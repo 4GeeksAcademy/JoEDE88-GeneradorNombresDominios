b4eaf3ac7086618ce21f0b9c889a9ee47ef72096
Joe <joediaz88@hotmail.com>
2024-11-06 20:39:12 +0000

    algoritmo para generar dominios creado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronombres = ["the", "our"];
+
+  let adjetivos = ["great", "big"];
+
+  let sustantivos = ["jogger", "racoon"];
+
+  let dominios = pronombres
+    .map(x => adjetivos.map(y => sustantivos.map(z => x + y + z).flat()).flat())
+    .flat();
+
+  console.log(dominios.map(i => i + ".com"));
 };
